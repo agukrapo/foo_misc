cee17faec4d07522caeb413470962cd92b14be30
Agustin Krapovickas <agukrapo@gmail.com>
2025-05-18 17:08:13 -0300

    fix: recycle file also when not paying from a playlist

--- src/mainmenu.cpp
+++ src/mainmenu.cpp
@@ -5,7 +5,7 @@ namespace {
 	static const GUID id_pl_rem = { 0x674c0096, 0xb9fb, 0x4f4d, { 0xa0, 0xf6, 0x93, 0x7b, 0x35, 0x3f, 0x99, 0xaf } };
 	static const GUID id_del = { 0x94eb3400, 0x0f40, 0x4c18, { 0xa8, 0x1b, 0x2b, 0xf2, 0x7d, 0xd0, 0xca, 0xa9 } };
 
-	static mainmenu_group_popup_factory g_mainmenu_group(id_group, mainmenu_groups::file, mainmenu_commands::sort_priority_dontcare, "Misc");
+	static mainmenu_group_popup_factory g_mainmenu_group(id_group, mainmenu_groups::playback, mainmenu_commands::sort_priority_dontcare, "Now playing");
 
 	class _commands : public mainmenu_commands {
 	public:
@@ -21,20 +21,20 @@ namespace {
 			switch (p_index) {
 			case cmd_pl_rem: return id_pl_rem;
 			case cmd_del_file: return id_del;
-			default: uBugCheck(); // should never happen
+			default: uBugCheck();
 			}
 		}
 		void get_name(t_uint32 p_index, pfc::string_base& p_out) override {
 			switch (p_index) {
 			case cmd_pl_rem: p_out = "Remove from playlist"; break;
 			case cmd_del_file: p_out = "Recycle file"; break;
-			default: uBugCheck(); // should never happen
+			default: uBugCheck();
 			}
 		}
 		bool get_description(t_uint32 p_index, pfc::string_base& p_out) override {
 			switch (p_index) {
-			case cmd_pl_rem: p_out = "Removes a song from playlist and plays the next one."; return true;
-			case cmd_del_file: p_out = "Deletes a song file and plays the next one."; return true;
+			case cmd_pl_rem: p_out = "Removes the currently playing song from its playlist and advances playback."; return true;
+			case cmd_del_file: p_out = "Moves the currently playing song to the trash bin and advances playback."; return true;
 			default: return false;
 			}
 		}
@@ -42,61 +42,64 @@ namespace {
 			return id_group;
 		}
 		void execute(t_uint32 p_index, service_ptr_t<service_base> p_callback) override {
-			auto pm = playlist_manager_v6::get();
+			switch (p_index) {
+			case cmd_pl_rem: exec_pl_rem(); break;
+			case cmd_del_file: exec_del_file(); break;
+			default:
+				uBugCheck();
+			}
+		}
 
+		void exec_pl_rem() {
+			auto pm = playlist_manager_v6::get();
 			t_size pl_idx, item_idx;
 			if (!pm->get_playing_item_location(&pl_idx, &item_idx)) {
 				return;
 			}
-
 			playback_control_v3::get()->next();
+			bit_array_one arr(item_idx);
+			pm->playlist_undo_backup(pl_idx);
+			pm->playlist_remove_items(pl_idx, arr);
+		}
 
-			switch (p_index) {
-			case cmd_pl_rem: {
+		void exec_del_file() {
+			auto pm = playlist_manager_v6::get();
+
+			t_size pl_idx, item_idx;
+			if (pm->get_playing_item_location(&pl_idx, &item_idx)) {
 				bit_array_one arr(item_idx);
-				pm->playlist_undo_backup(pl_idx);
 				pm->playlist_remove_items(pl_idx, arr);
-
-				break;
 			}
 
-			case cmd_del_file: {
-				auto item = pm->playlist_get_item_handle(pl_idx, item_idx);
+			auto pc = playback_control_v3::get();
 
-				if (!item.is_valid() || item.is_empty()) {
-					popup_message::g_complain("Could not recycle", "Item is invalid");
-					return;
-				}
-
-				std::string str(item->get_path());
-				if (str._Starts_with("file://")) {
-					str.erase(0, 7);
-				}
-
-				CA2W ca2w(str.c_str());
-				std::wstring wstr = ca2w;
-
-				wstr += std::wstring(1, L'\0');
-
-				SHFILEOPSTRUCT fileOp = { 0 };
-				fileOp.wFunc = FO_DELETE;
-				fileOp.pFrom = wstr.c_str();
-				fileOp.fFlags = FOF_ALLOWUNDO;
-				
-				int code = SHFileOperation(&fileOp);
-				if (code == 0) {
-					bit_array_one arr(item_idx);
-					pm->playlist_remove_items(pl_idx, arr);
-				} else {
-					pfc::string_formatter msg;
-					msg << "Error code " << code << "\nwstr.c_str(): " << wstr.c_str() << "\nstr: " << str.c_str();
-					popup_message::g_complain("Could not recycle", msg);
-				}
-
-				break;
+			metadb_handle_ptr item;
+			if (!pc->get_now_playing(item)) {
+				return;
 			}
-			default:
-				uBugCheck();
+
+			pc->next();
+
+			std::string str(item->get_path());
+			if (str._Starts_with("file://")) {
+				str.erase(0, 7);
+			}
+
+			CA2W ca2w(str.c_str());
+			std::wstring wstr = ca2w;
+
+			wstr += std::wstring(1, L'\0');
+
+			SHFILEOPSTRUCT fileOp = { 0 };
+			fileOp.wFunc = FO_DELETE;
+			fileOp.pFrom = wstr.c_str();
+			fileOp.fFlags = FOF_ALLOWUNDO;
+
+			int code = SHFileOperation(&fileOp);
+			if (code != 0) {
+				pfc::string_formatter msg;
+				msg << "Error code " << code << "\nwstr.c_str(): " << wstr.c_str() << "\nstr: " << str.c_str();
+				popup_message::g_complain("Could not recycle", msg);
 			}
 		}
 	};
