b1dae2c24baf43a04da7074919fe7f2eab11d82f
Agustin Krapovickas <agukrapo@gmail.com>
2025-07-16 14:11:01 -0300

    feat: copy artist md context menu item

--- src/context_copy_md.cpp
+++ src/context_copy_md.cpp
@@ -10,9 +10,10 @@ namespace {
 	public:
 		enum {
 			cmd_song = 0,
-			cmd_artist_album,
-			cmd_album,
 			cmd_title,
+			cmd_artist,
+			cmd_album,
+			cmd_artist_album,
 			cmd_directory,
 			cmd_path,
 			cmd_total
@@ -29,9 +30,10 @@ namespace {
 		void get_item_name(unsigned p_index, pfc::string_base& p_out) override {
 			switch (p_index) {
 			case cmd_song: p_out = "Song"; break;
-			case cmd_artist_album: p_out = "Artist Album"; break;
-			case cmd_album: p_out = "Album"; break;
 			case cmd_title: p_out = "Title"; break;
+			case cmd_artist: p_out = "Artist"; break;
+			case cmd_album: p_out = "Album"; break;
+			case cmd_artist_album: p_out = "Artist Album"; break;
 			case cmd_directory: p_out = "Directory"; break;
 			case cmd_path: p_out = "Path"; break;
 			default: uBugCheck();
@@ -40,17 +42,19 @@ namespace {
 
 		GUID get_item_guid(unsigned p_index) override {
 			static const GUID id_cmd_song = { 0x3bc114ac, 0x207d, 0x4a3c, { 0xa1, 0x6e, 0xfe, 0xe0, 0x62, 0x39, 0xb1, 0x6a } };
-			static const GUID id_cmd_artist_album = { 0x6798346d, 0xdf7f, 0x4d4a, { 0x8b, 0x20, 0xea, 0xd3, 0x69, 0xb2, 0xb2, 0xe3 } };
-			static const GUID id_cmd_album = { 0x2e21e5fd, 0xf5d1, 0x40c4, { 0x81, 0x70, 0xe2, 0x80, 0x4f, 0xf9, 0x18, 0xdd } };
 			static const GUID id_cmd_title = { 0x3475a715, 0x7cb3, 0x46ee, { 0xb3, 0x67, 0xef, 0x12, 0xd5, 0x99, 0x17, 0x26 } };
+			static const GUID id_cmd_artist = { 0xaa40d55c, 0xb9ff, 0x417f, { 0xaa, 0xae, 0xe, 0x73, 0x9d, 0x88, 0x23, 0xbd } };
+			static const GUID id_cmd_album = { 0x2e21e5fd, 0xf5d1, 0x40c4, { 0x81, 0x70, 0xe2, 0x80, 0x4f, 0xf9, 0x18, 0xdd } };
+			static const GUID id_cmd_artist_album = { 0x6798346d, 0xdf7f, 0x4d4a, { 0x8b, 0x20, 0xea, 0xd3, 0x69, 0xb2, 0xb2, 0xe3 } };
 			static const GUID id_cmd_directory = { 0x7484b70f, 0x90c1, 0x453c, { 0x80, 0x77, 0xbf, 0x9a, 0xb0, 0x51, 0x4f, 0xba } };
 			static const GUID id_cmd_path = { 0x44d6a8c6, 0x313e, 0x45a9, { 0x8e, 0x98, 0xc6, 0x9c, 0x15, 0x5d, 0x31, 0x17 } };
 
 			switch (p_index) {
 			case cmd_song: return id_cmd_song;
-			case cmd_artist_album: return id_cmd_artist_album;
-			case cmd_album: return id_cmd_album;
 			case cmd_title: return id_cmd_title;
+			case cmd_artist: return id_cmd_artist;
+			case cmd_album: return id_cmd_album;
+			case cmd_artist_album: return id_cmd_artist_album;
 			case cmd_directory: return id_cmd_directory;
 			case cmd_path: return id_cmd_path;
 			default: uBugCheck();
@@ -60,9 +64,10 @@ namespace {
 		bool get_item_description(unsigned p_index, pfc::string_base& p_out) override {
 			switch (p_index) {
 			case cmd_song: p_out = "Copies the song name."; return true;
-			case cmd_artist_album: p_out = "Copies artist & album names."; return true;
-			case cmd_album: p_out = "Copies the album name."; return true;
 			case cmd_title: p_out = "Copies the title."; return true;
+			case cmd_artist: p_out = "Copies the artist name."; return true;
+			case cmd_album: p_out = "Copies the album name."; return true;
+			case cmd_artist_album: p_out = "Copies artist & album names."; return true;
 			case cmd_directory: p_out = "Copies the directory."; return true;
 			case cmd_path: p_out = "Copies the file path."; return true;
 			default:
@@ -73,9 +78,10 @@ namespace {
 		void context_command(unsigned p_index, metadb_handle_list_cref p_data, const GUID& p_caller) override {
 			switch (p_index) {
 			case cmd_song: metaCopy(p_data, "[[%artist%] [%title%]]"); break;
-			case cmd_artist_album: metaCopy(p_data, "[[%artist%] [%album%]]"); break;
-			case cmd_album: metaCopy(p_data, "[%album%]"); break;
 			case cmd_title: metaCopy(p_data, "[%title%]"); break;
+			case cmd_artist: metaCopy(p_data, "[%artist%]"); break;
+			case cmd_album: metaCopy(p_data, "[%album%]"); break;
+			case cmd_artist_album: metaCopy(p_data, "[[%artist%] [%album%]]"); break;
 			case cmd_directory: metaCopy(p_data, "%directory%"); break;
 			case cmd_path: metaCopy(p_data, "%path%"); break;
 			default:
