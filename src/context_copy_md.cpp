eb82b32fc1bc48afc0cf7ce7899866df0384446d
Agustin Krapovickas <agukrapo@gmail.com>
2025-07-16 14:55:34 -0300

    fix: folder copy md context menu

--- src/context_copy_md.cpp
+++ src/context_copy_md.cpp
@@ -14,8 +14,7 @@ namespace {
 			cmd_artist,
 			cmd_album,
 			cmd_artist_album,
-			cmd_directory,
-			cmd_path,
+			cmd_folder,
 			cmd_total
 		};
 
@@ -34,8 +33,7 @@ namespace {
 			case cmd_artist: p_out = "Artist"; break;
 			case cmd_album: p_out = "Album"; break;
 			case cmd_artist_album: p_out = "Artist Album"; break;
-			case cmd_directory: p_out = "Directory"; break;
-			case cmd_path: p_out = "Path"; break;
+			case cmd_folder: p_out = "Folder"; break;
 			default: uBugCheck();
 			}
 		}
@@ -46,8 +44,7 @@ namespace {
 			static const GUID id_cmd_artist = { 0xaa40d55c, 0xb9ff, 0x417f, { 0xaa, 0xae, 0xe, 0x73, 0x9d, 0x88, 0x23, 0xbd } };
 			static const GUID id_cmd_album = { 0x2e21e5fd, 0xf5d1, 0x40c4, { 0x81, 0x70, 0xe2, 0x80, 0x4f, 0xf9, 0x18, 0xdd } };
 			static const GUID id_cmd_artist_album = { 0x6798346d, 0xdf7f, 0x4d4a, { 0x8b, 0x20, 0xea, 0xd3, 0x69, 0xb2, 0xb2, 0xe3 } };
-			static const GUID id_cmd_directory = { 0x7484b70f, 0x90c1, 0x453c, { 0x80, 0x77, 0xbf, 0x9a, 0xb0, 0x51, 0x4f, 0xba } };
-			static const GUID id_cmd_path = { 0x44d6a8c6, 0x313e, 0x45a9, { 0x8e, 0x98, 0xc6, 0x9c, 0x15, 0x5d, 0x31, 0x17 } };
+			static const GUID id_cmd_folder = { 0x7484b70f, 0x90c1, 0x453c, { 0x80, 0x77, 0xbf, 0x9a, 0xb0, 0x51, 0x4f, 0xba } };
 
 			switch (p_index) {
 			case cmd_song: return id_cmd_song;
@@ -55,8 +52,7 @@ namespace {
 			case cmd_artist: return id_cmd_artist;
 			case cmd_album: return id_cmd_album;
 			case cmd_artist_album: return id_cmd_artist_album;
-			case cmd_directory: return id_cmd_directory;
-			case cmd_path: return id_cmd_path;
+			case cmd_folder: return id_cmd_folder;
 			default: uBugCheck();
 			}
 		}
@@ -68,8 +64,7 @@ namespace {
 			case cmd_artist: p_out = "Copies the artist name."; return true;
 			case cmd_album: p_out = "Copies the album name."; return true;
 			case cmd_artist_album: p_out = "Copies artist & album names."; return true;
-			case cmd_directory: p_out = "Copies the directory."; return true;
-			case cmd_path: p_out = "Copies the file path."; return true;
+			case cmd_folder: p_out = "Copies the folder name."; return true;
 			default:
 				uBugCheck();
 			}
@@ -77,19 +72,18 @@ namespace {
 
 		void context_command(unsigned p_index, metadb_handle_list_cref p_data, const GUID& p_caller) override {
 			switch (p_index) {
-			case cmd_song: metaCopy(p_data, "[[%artist%] [%title%]]"); break;
-			case cmd_title: metaCopy(p_data, "[%title%]"); break;
-			case cmd_artist: metaCopy(p_data, "[%artist%]"); break;
-			case cmd_album: metaCopy(p_data, "[%album%]"); break;
-			case cmd_artist_album: metaCopy(p_data, "[[%artist%] [%album%]]"); break;
-			case cmd_directory: metaCopy(p_data, "%directory%"); break;
-			case cmd_path: metaCopy(p_data, "%path%"); break;
+			case cmd_song: titleformatCopy(p_data, "[[%artist%] [%title%]]"); break;
+			case cmd_title: titleformatCopy(p_data, "[%title%]"); break;
+			case cmd_artist: titleformatCopy(p_data, "[%artist%]"); break;
+			case cmd_album: titleformatCopy(p_data, "[%album%]"); break;
+			case cmd_artist_album: titleformatCopy(p_data, "[[%artist%] [%album%]]"); break;
+			case cmd_folder: titleformatCopy(p_data, "$directory_path(%path%)"); break;
 			default:
 				uBugCheck();
 			}
 		}
 
-		void metaCopy(metadb_handle_list_cref p_data, const char* script) {
+		void titleformatCopy(metadb_handle_list_cref p_data, const char* script) {
 			pfc::avltree_t<pfc::string> dedups;
 			for (t_size i = 0; i < p_data.get_count(); ++i) {
 				auto fmt = fb2k::formatTrackTitle(p_data[i], script);
